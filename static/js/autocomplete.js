e947a865e5456a33aabde6bb54cd6355dfe53852
Victor <victorc1165@yahoo.com>
2017-12-30 02:18:04 -0800

    update gitignore and cleanup code

--- static/js/autocomplete.js
+++ static/js/autocomplete.js
@@ -1,21 +1,16 @@
-var req_url = 'http://api.glassdoor.com/api/api.htm?v=';
-
-function getCompany(name){
-  console.log({company: name})
+function autocomplete(company){
   return $.ajax({
     url: '/acProxy',
     type: 'POST',
-    data: name,
+    data: company,
     dataType: 'json',
     contentType: 'string',
     success: function(data){
-      console.log(data);
-      console.log(typeof(data));
       var autocomplete_options = [];
       for (var key in data) {
         autocomplete_options.push(data[key]);
       }
-      console.log(autocomplete_options);
+      #console.log(autocomplete_options);
       $("#companyinput").autocomplete({
         source: autocomplete_options
       });
@@ -23,14 +18,7 @@ function getCompany(name){
   });
 }
 
-
-function buildURL(v, format, tp, tk, action, userip, useragent, searchterm){
-    return (req_url + v + "&format=" + format + "&t.p=" + tp + "&t.k=" + tk +
-         "&action=" + action + "&q=" + searchterm + "&userip=" + userip +
-         "&useragent=" + useragent);
-}
-
 $("#companyinput").on('input', function(){
-  var str = $(this)[0].value;
-  getCompany(str);
+  var company = $(this)[0].value;
+  autocomplete(company);
 });
